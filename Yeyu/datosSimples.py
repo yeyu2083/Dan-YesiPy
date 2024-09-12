356751dcfe5cb415ea8e187946b7148d75bb3ac4
yeyu2083 <ywindecker@gmail.com>
2024-09-11 21:30:17 -0300

    datos simples y compuestos

---
+++ Yeyu/datosSimples.py
@@ -0,0 +1,17 @@
+#declaramos una variable
+nombre = 'Yesi'
+
+#concatenamos simple
+bienvenido = 'Hola ' + nombre + ' Como estas?'
+
+#f string es para cambiar cualquier valor a texto
+bienvenida = f'Hola {nombre} Como Estas?'
+
+#operadores de pertenencia
+
+print(bienvenida)
+print(bienvenido)
+
+print('Hola' in bienvenida) #true
+
+print('Yesi' not in bienvenida) #false
