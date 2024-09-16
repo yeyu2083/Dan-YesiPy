c0afbba5819a15d222ec55d0dd54f71bda371cd2
yeyu2083 <ywindecker@gmail.com>
2024-09-15 22:51:43 -0300

    op de comparacion y condicionales, ademas coloque una variable de entorno para que tomara las ñ

---
+++ Yeyu/condicionales.py
@@ -0,0 +1,20 @@
+#-*- coding: utf-8 -*-
+#if condicion = accion (true) se ejecuta
+
+# edad = 17
+
+# if edad >= 15:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de edad")
+
+
+contraseña_establecida = "Windecker Yesi"
+
+contraseña_almacenada = "windecker Yesi"
+
+if contraseña_establecida == contraseña_almacenada:
+    print("Contraseña correcta")
+else:
+    print("Contraseña incorrecta")
+    
---
+++ Yeyu/operadores_de_comparacion.py
@@ -0,0 +1,20 @@
+#un valor A y un valor B
+
+es_igual_que = 5 == 6
+
+distinto_de = 5 != 6 
+
+mayor_que = 5 > 6
+
+menor_que  = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+
+contraseña_usuario = "Yesi Windecker"
+
+contraseña_ingresada = "Windecker Yesi"
+ 
+
+print(contraseña_usuario == contraseña_ingresada)
